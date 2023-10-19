4ac7a8f1ab9723d66540090beaafd6682991979d
njizzle <87454396+njizzle@users.noreply.github.com>
2023-10-19 22:34:55 +0100

    Add files via upload

---
+++ Day 1/bandname.py
@@ -0,0 +1,7 @@
+print("Hey Im going to create you a band name")
+
+area = input("What town/city did you grow up \n")
+
+pet = input("what animal was the first pet you had \n")
+
+print(f"your band name is {area} {pet} hope you like it")
